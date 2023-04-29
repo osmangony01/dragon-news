40888551cdb78fcbb37100775181752e4689bcd1
osmangony01 <mdosmangoni376@gmail.com>
2023-04-29 13:04:17 +0600

    add some content in right-nav

--- src/components/Home/RightContent/RightContent.jsx
+++ src/components/Home/RightContent/RightContent.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import bgImage from "../../../../public/images/bg.png"
 
 const RightContent = () => {
     return (
@@ -40,6 +41,12 @@ const RightContent = () => {
                 <img src="/images/qZone1.png" alt="" />
                 <img src="/images/qZone2.png" alt="" />
                 <img src="/images/qZone3.png" alt="" />
+            </div>
+
+            <div className='text-center px-5 py-10 feature1 text-white mt-4'>
+                <h2 className='text-lg font-bold'>Create an Amazing Newspaper</h2>
+                <p className='my-4'>Discover thousands of options, easy to customize layouts, one-click to import demo and much more.</p>
+                <button className='text-white bg-red-600 p-2'>Learn More</button>
             </div>
         </>
     );
